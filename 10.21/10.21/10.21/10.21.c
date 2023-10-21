9d97acbe50abe929d8e8813da051929931680c49
志木心夏 <147913038+ShikiKokona@users.noreply.github.com>
2023-10-21 16:59:01 +0800

    Add files via upload

---
+++ 10.21/10.21/10.21/10.21.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int i = 1;
+//
+//	while (i < 10)
+//	{
+//		printf("%d ", i);
+//		i++;
+//	}
+//
+//	return 0;
+//}
+
+
+
+//打印1~10
+
+//int main()
+//{
+//	int i;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		if (i == 5)
+//			//break;
+//			continue;
+//		printf("%d ", i);
+//	}
+//
+//	return 0;
+//}
+
+
+int main()
+{
+	//for循环的判断部分省略意味着判断会恒成立
+	for (;;)
+	{
+		printf("hehe\n");
+	}
+
+	return 0;
+}
